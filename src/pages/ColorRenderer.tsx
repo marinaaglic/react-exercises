8e540403b36d5bc281ed95d9b02703c16f52e1fd
jspvg <josipovic1501@gmail.com>
2023-11-09 21:02:55 +0100

    added task in comment and finished example image

--- src/pages/ColorRenderer.tsx
+++ src/pages/ColorRenderer.tsx
@@ -1,23 +1,29 @@
-/*import Color from "../components/Color";
+/* TASK
+Build a color renderer that will display color swatches on a user interface
+Update the ColorRenderer component to display one instance of the 
+Color component for each color in the array of colors
+*/
 
- const colors = [
-  {
-    hex: "#91A6FF",
-    name: "Cornflower Blue",
-  },
-  {
-    hex: "#FF88DC",
-    name: "Persian Pink",
-  },
-  {
-    hex: "#80FF72",
-    name: "Screamin Green",
-  },
-  {
-    hex: "#FF5154",
-    name: "Tart Orange",
-  }, 
-];*/
+// import Color from "../components/Color";
+
+//  const colors = [
+//   {
+//     hex: "#91A6FF",
+//     name: "Cornflower Blue",
+//   },
+//   {
+//     hex: "#FF88DC",
+//     name: "Persian Pink",
+//   },
+//   {
+//     hex: "#80FF72",
+//     name: "Screamin Green",
+//   },
+//   {
+//     hex: "#FF5154",
+//     name: "Tart Orange",
+//   },
+// ];
 
 const ColorRenderer = () => {
   return (
