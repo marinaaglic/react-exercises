02210e8133e7008635f84766b2003ec921cd45c3
jspvg <josipovic1501@gmail.com>
2023-11-09 21:57:43 +0100

    initialized color-picker exercise with task and example result

---
+++ src/pages/ColorPicker.tsx
@@ -0,0 +1,41 @@
+/* TASK
+Create a color picking app so the user can select the page's background color
+Requirements:
+- Upon clicking a color, change the whole page's background to be that color
+- Make sure to use the Color subcomponent
+*/
+
+import { useState } from "react";
+import Color from "../components/Color";
+
+const colors = [
+  {
+    hex: "#91A6FF",
+    name: "Cornflower Blue",
+  },
+  {
+    hex: "#FF88DC",
+    name: "Persian Pink",
+  },
+  {
+    hex: "#80FF72",
+    name: "Screamin Green",
+  },
+  {
+    hex: "#FF5154",
+    name: "Tart Orange",
+  },
+];
+
+export default function ColorPicker() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [backgroundColor, setBackgroundColor] = useState("white");
+
+  return (
+    <div className="page" style={{ backgroundColor }}>
+      {colors.map((color) => (
+        <Color key={color.hex} hex={color.hex} name={color.name} />
+      ))}
+    </div>
+  );
+}
