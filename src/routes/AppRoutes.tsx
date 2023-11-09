6c1ab797b59eb9d2c678466b668579d9d44b84e9
jspvg <josipovic1501@gmail.com>
2023-11-09 21:22:52 +0100

    initialized score-keeper exercise with task

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import ColorRenderer from "../pages/ColorRenderer";
+import ScoreKeeper from "../pages/ScoreKeeper";
 
 const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="*" element={<Home />} />
         <Route path="/color-renderer" element={<ColorRenderer />} />
+        <Route path="/score-keeper" element={<ScoreKeeper />} />
       </Routes>
     </div>
   );
