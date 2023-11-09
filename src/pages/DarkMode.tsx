1b51bc3a2f9546336663e2ec9774116ff45ee37f
jspvg <josipovic1501@gmail.com>
2023-11-09 20:18:18 +0100

    initialized dark-mode exercise

---
+++ src/pages/DarkMode.tsx
@@ -0,0 +1,10 @@
+const DarkMode = () => {
+  return (
+    <div className="page">
+      <button className="dark-mode-button">Dark Mode</button>
+      <button className="light-mode-button">Light Mode</button>
+    </div>
+  );
+};
+
+export default DarkMode;
