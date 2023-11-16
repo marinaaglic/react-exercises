279bbdd3d072c920d55863c7241c00e2410b662c
jspvg <josipovic1501@gmail.com>
2023-11-16 20:34:19 +0100

    finished form validator task

--- src/pages/FormValidator.tsx
+++ src/pages/FormValidator.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 /* TASK 
-Validate a user sign-up formonce the user submits the form
+Validate a user sign-up form once the user submits the form
 When the form is submitted, validate that the user's information fits the following requirements:
 -the email, password, and password confirmation fields are filled in
 -the email field must have exactly one @sign in it
@@ -15,26 +15,67 @@ const FormValidator = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!email && !password && !passwordConfirm) {
+      setError("Fields must not be empty.");
+      setSuccess("");
+      return;
+    }
+
+    if (!email || email.indexOf("@") === -1) {
+      setError("Invalid email address.");
+      setSuccess("");
+      return;
+    }
+
+    if (password.length < 8) {
+      setError("Password must be at least 8 characters long.");
+      setSuccess("");
+      return;
+    }
+
+    if (password !== passwordConfirm) {
+      setError("Password confirmation must be the same as the password.");
+      setSuccess("");
+      return;
+    }
+
+    setError("");
+    setSuccess("Form submitted!");
+    setEmail("");
+    setPassword("");
+    setPasswordConfirm("");
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
+      {success && <p>{success}</p>}
       <label htmlFor="email">Email</label>
       <input
-        type="text"
-        name="email"
+        type="email"
+        id="email"
+        value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <label htmlFor="password">Password</label>
       <input
         type="password"
-        name="password"
+        id="password"
+        value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
       <label htmlFor="password-confirm">Confirm Password </label>
       <input
         type="password"
-        name="password-confirm"
+        id="password-confirm"
+        value={passwordConfirm}
         onChange={(e) => setPasswordConfirm(e.target.value)}
       />
       <input type="submit" value="Submit" />
