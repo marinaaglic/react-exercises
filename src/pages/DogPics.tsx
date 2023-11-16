ac95e596121903097ebf6167c2d65ac6241e4f0f
jspvg <josipovic1501@gmail.com>
2023-11-16 21:16:56 +0100

    finished dog pictures task

--- src/pages/DogPics.tsx
+++ src/pages/DogPics.tsx
@@ -2,16 +2,51 @@
 Use a dog picture API to display dog images in React
 Reaquirements:
 -Use the dog.ceo to fetch a random dog image and display 
-    that image on page load instead of the hardcoded one there now
+  that image on page load instead of the hardcoded one there now
 -When the dog button is clicked, fetch a new dog image and render it on the UI instead
 */
 
+import { useEffect, useState } from "react";
+
+type PictureResponse = {
+  message: string;
+  status: string;
+};
+
 const DogPics = () => {
   // API: https://dog.ceo/dog-api/
+  const [image, setImage] = useState<PictureResponse>({
+    message: "https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg",
+    status: "success",
+  });
+  const [click, setClick] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data.");
+        }
+        const data: PictureResponse = await response.json();
+        setImage(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, [click]);
+
+  const handleClick = () => {
+    const newClick = click + 1;
+    setClick(newClick);
+  };
+
   return (
     <div className="dog-pics">
-      <img src="https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg" />
-      <button>🐶</button>
+      <img src={image.message} />
+      <button onClick={handleClick}>🐶</button>
     </div>
   );
 };
