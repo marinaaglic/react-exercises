1fa845c50130cff4555e56d6b1d954d96f3e8869
jspvg <josipovic1501@gmail.com>
2023-11-16 19:40:22 +0100

    finished dark mode task

--- src/pages/DarkMode.tsx
+++ src/pages/DarkMode.tsx
@@ -3,11 +3,29 @@ Implement a component that allows you to change from light to
 dark mode and back again
 */
 
+import { useState } from "react";
+
 const DarkMode = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  const handleDarkMode = () => {
+    setIsDark(true);
+  };
+
+  const handleLightMode = () => {
+    setIsDark(false);
+  };
+
+  const pageClass = `page ${isDark ? "dark-mode" : ""}`;
+
   return (
-    <div className="page">
-      <button className="dark-mode-button">Dark Mode</button>
-      <button className="light-mode-button">Light Mode</button>
+    <div className={pageClass}>
+      <button className="dark-mode-button" onClick={handleDarkMode}>
+        Dark Mode
+      </button>
+      <button className="light-mode-button" onClick={handleLightMode}>
+        Light Mode
+      </button>
     </div>
   );
 };
