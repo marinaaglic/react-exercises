8bd71861d0fd25252aca86cf69abfd2165e5a83f
jspvg <josipovic1501@gmail.com>
2023-11-16 19:22:52 +0100

    finished task

--- src/pages/ColorRenderer.tsx
+++ src/pages/ColorRenderer.tsx
@@ -4,34 +4,33 @@ Update the ColorRenderer component to display one instance of the
 Color component for each color in the array of colors
 */
 
-// import Color from "../components/Color";
+import Color from "../components/Color";
 
-//  const colors = [
-//   {
-//     hex: "#91A6FF",
-//     name: "Cornflower Blue",
-//   },
-//   {
-//     hex: "#FF88DC",
-//     name: "Persian Pink",
-//   },
-//   {
-//     hex: "#80FF72",
-//     name: "Screamin Green",
-//   },
-//   {
-//     hex: "#FF5154",
-//     name: "Tart Orange",
-//   },
-// ];
-
-const ColorRenderer = () => {
-  return (
-    <h2>
-      Use the Color component to render each item in the colors array on the
-      page
-    </h2>
-  );
+type Color = {
+  hex: string;
+  name: string;
 };
+
+const colors: Color[] = [
+  {
+    hex: "#91A6FF",
+    name: "Cornflower Blue",
+  },
+  {
+    hex: "#FF88DC",
+    name: "Persian Pink",
+  },
+  {
+    hex: "#80FF72",
+    name: "Screamin Green",
+  },
+  {
+    hex: "#FF5154",
+    name: "Tart Orange",
+  },
+];
+
+const ColorRenderer = () =>
+  colors.map((color) => <Color hex={color.hex} name={color.name} />);
 
 export default ColorRenderer;
