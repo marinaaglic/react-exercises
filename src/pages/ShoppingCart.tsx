eb059febf3771e131d1910ebaadf6f672dc7e2b4
jspvg <josipovic1501@gmail.com>
2023-11-20 17:00:11 +0100

    finished shopping cart component, using usereducer

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,14 +1,13 @@
 /* TASK 
-Build a shopping cart that displays total price when adding items.
-Requireents:
+Build a shopping cart that displays the total price when adding items.
+Requirements:
 - Implement the Add to Cart logic, add an item to the cart when the
   button is clicked
 - Implement the + and - buttons so that you can add or remove items from the cart
 - Implement the total sum at the bottom of the page
 */
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React, { useState } from "react";
+import { useReducer } from "react";
 
 const items = [
   {
@@ -25,8 +24,62 @@ const items = [
   },
 ];
 
+type Item = {
+  name: string;
+  quantity?: number;
+  price: number;
+};
+
+type CartAction =
+  | { type: "add_item"; item: Item }
+  | { type: "increment_quantity"; itemName: string }
+  | { type: "decrement_quantity"; itemName: string };
+
+const cartReducer = (state: Item[], action: CartAction): Item[] => {
+  switch (action.type) {
+    case "add_item":
+      return [...state, { ...action.item, quantity: 1 }];
+    case "increment_quantity":
+      return state.map((item) =>
+        item.name === action.itemName
+          ? { ...item, quantity: (item.quantity || 0) + 1 }
+          : item
+      );
+    case "decrement_quantity":
+      return state
+        .map((item) =>
+          item.name === action.itemName
+            ? { ...item, quantity: Math.max(0, (item.quantity || 0) - 1) }
+            : item
+        )
+        .filter((item) => item.quantity !== 0);
+  }
+  return state;
+};
+
 const ShoppingCart = () => {
-  const cart = [{ name: "apple", quantity: 3, price: 0.39 }];
+  const [cart, dispatch] = useReducer(cartReducer, []);
+
+  const handleAddToCart = (newItem: Item) => {
+    const alreadyExists = cart.find((item) => item.name === newItem.name);
+    if (alreadyExists) {
+      dispatch({ type: "increment_quantity", itemName: newItem.name });
+    } else {
+      dispatch({ type: "add_item", item: newItem });
+    }
+  };
+
+  const handleIncrement = (itemName: string) => {
+    dispatch({ type: "increment_quantity", itemName: itemName });
+  };
+
+  const handleDecrement = (itemName: string) => {
+    dispatch({ type: "decrement_quantity", itemName: itemName });
+  };
+
+  const total = cart
+    .reduce((prev, item) => prev + item.price * item.quantity!, 0)
+    .toFixed(2);
 
   return (
     <div>
@@ -38,7 +91,7 @@ const ShoppingCart = () => {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button>Add to Cart</button>
+              <button onClick={() => handleAddToCart(item)}>Add to Cart</button>
             </div>
           ))}
         </div>
@@ -48,17 +101,17 @@ const ShoppingCart = () => {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>
-                <button>-</button>
+                <button onClick={() => handleDecrement(item.name)}>-</button>
                 {item.quantity}
-                <button>+</button>
+                <button onClick={() => handleIncrement(item.name)}>+</button>
               </p>
-              <p>Subtotal: ${item.quantity * item.price}</p>
+              <p>Subtotal: ${(item.quantity! * item.price).toFixed(2)}</p>
             </div>
           ))}
         </div>
       </div>
       <div className="total">
-        <h2>Total: $0.00</h2>
+        <h2>Total: {total}</h2>
       </div>
     </div>
   );
