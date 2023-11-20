df901d3d502d39d0e78a518844bc1e73c7f7b6bb
jspvg <josipovic1501@gmail.com>
2023-11-20 14:26:52 +0100

    finished PixelArt component using useContext

--- src/pages/PixelArt.tsx
+++ src/pages/PixelArt.tsx
@@ -4,59 +4,26 @@ React Context
 -   Allows you to share data between components more easily. 
     Instead of having to pass a prop down multiple levels, you can use Context.
 Requirements:
-- When a color is clicked, taht color becomes the drawing color. Then, when one of the grid
+- When a color is clicked, that color becomes the drawing color. Then, when one of the grid
     squares is clicked, it should be colored in with the drawing color.
 - Use React Context to share state between your components
 */
 
-const ColorPicker = () => {
-  const colors = ["red", "blue", "yellow", "green", "black", "white", "purple"];
-  return (
-    <div>
-      <h1>Choose a color</h1>
-      {colors.map((color) => (
-        <button key={color} style={{ backgroundColor: color }} />
-      ))}
-    </div>
-  );
-};
-
-const Pixel = () => {
-  return (
-    <div
-      style={{
-        height: "20px",
-        width: "20px",
-        backgroundColor: "lightGrey",
-        margin: "1px",
-      }}
-    />
-  );
-};
-
-const Pixels = () => {
-  const pixels = [];
-  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />);
-  return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "repeat(10, 1fr)",
-        width: "210px",
-        margin: "0 auto",
-      }}
-    >
-      {pixels}
-    </div>
-  );
-};
+import { useState } from "react";
+import { DrawingColorContext } from "../hooks/useDrawingColor";
+import PixelColorPicker from "../components/PixelArt/PixelColorPicker";
+import Pixels from "../components/PixelArt/Pixels";
 
 const PixelArt = () => {
+  const [drawingColor, setDrawingColor] = useState("black");
+
   return (
-    <div>
-      <ColorPicker />
-      <Pixels />
-    </div>
+    <DrawingColorContext.Provider value={{ drawingColor, setDrawingColor }}>
+      <div>
+        <PixelColorPicker />
+        <Pixels />
+      </div>
+    </DrawingColorContext.Provider>
   );
 };
 
