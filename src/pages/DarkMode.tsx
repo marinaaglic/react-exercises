383476337beeb3464e8fffd7a772cf6e8853b1d1
marinaaglic <marinaaglic11@gmail.com>
2024-05-11 18:33:59 +0200

    finished task dark-mode

--- src/pages/DarkMode.tsx
+++ src/pages/DarkMode.tsx
@@ -1,8 +1,30 @@
+/* TASK
+Implement a component that allows you to change from light to 
+dark mode and back again
+*/
+
+import { useState } from "react";
+import "../index.css";
+
 const DarkMode = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(true);
+  };
+
+  const toggleLightMode = () => {
+    setDarkMode(false);
+  };
+
   return (
-    <div className="page">
-      <button className="dark-mode-button">Dark Mode</button>
-      <button className="light-mode-button">Light Mode</button>
+    <div className={`page ${darkMode ? "dark-mode" : ""}`}>
+      <button className="dark-mode-button" onClick={toggleDarkMode}>
+        Dark Mode
+      </button>
+      <button className="light-mode-button" onClick={toggleLightMode}>
+        Light Mode
+      </button>
     </div>
   );
 };
