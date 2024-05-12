f6533c63ed2aca9735939a4d864a199a19cc985b
marinaaglic <marinaaglic11@gmail.com>
2024-05-12 18:16:01 +0200

    finished task focus-input

--- src/pages/FocusInput.tsx
+++ src/pages/FocusInput.tsx
@@ -5,11 +5,22 @@ Requirements:
 - Upon page load, focus the input
 */
 
-const FocusInput = () => (
-  <div>
-    <label htmlFor="focused-input">Focus me on page load!</label>
-    <input name="focused-input"></input>
-  </div>
-);
+import { useEffect, useRef } from "react";
+
+const FocusInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+  return (
+    <div>
+      <label htmlFor="focused-input">Focus me on page load!</label>
+      <input name="focused-input" ref={inputRef}></input>
+    </div>
+  );
+};
 
 export default FocusInput;
