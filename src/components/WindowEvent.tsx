6996a77f5980494a2cad1d8c2303a4ef09fdccdd
marinaaglic <marinaaglic11@gmail.com>
2024-05-26 23:05:08 +0200

    finished task window-event

--- src/components/WindowEvent.tsx
+++ src/components/WindowEvent.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 const WindowEvent = () => {
+  const doubleClickHandler = () => {
+    alert("Double click.");
+  };
+
+  useEffect(() => {
+    window.addEventListener("dblclick", doubleClickHandler);
+    return () => {
+      window.removeEventListener("dblclick", doubleClickHandler);
+    };
+  }, []);
   return <h2>Window event active</h2>;
 };
 
