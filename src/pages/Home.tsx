c1b69626ac5f3d68aa2763c60f212a017d6c88de
marinaaglic <marinaaglic11@gmail.com>
2024-06-02 19:23:57 +0200

    added a link for each of the task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="navbar-home">
-      <h2>Homepage</h2>;
+      <Link to="/">Home</Link>
+      <Link to="/color-renderer">Color Renderer</Link>
+      <Link to="/color-picker">Color Picker</Link>
+      <Link to="/dark-mode">Dark Mode</Link>
+      <Link to="/dog-pics">Dog Pics</Link>
+      <Link to="/focus-input">Focus Input</Link>
+      <Link to="/form-validator">Form Validator</Link>
+      <Link to="/pixel-art">Pixel Art</Link>
+      <Link to="/score-keeper">Score Keeper</Link>
+      <Link to="/shopping-cart">Shopping Cart</Link>
+      <Link to="/simple-calculator">Simple Calculator</Link>
+      <h2>Homepage</h2>
     </div>
   );
 };
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,14 +2,30 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import ColorRenderer from "../pages/ColorRenderer";
 import ColorPicker from "../pages/ColorPicker";
+import DarkMode from "../pages/DarkMode";
+import DogPics from "../pages/DogPics";
+import FocusInput from "../pages/FocusInput";
+import FormValidator from "../pages/FormValidator";
+import PixelArt from "../pages/PixelArt";
+import ScoreKeeper from "../pages/ScoreKeeper";
+import ShoppingCart from "../pages/ShoppingCart";
+import SimpleCalculator from "../pages/SimpleCalculator";
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route path="*" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/color-renderer" element={<ColorRenderer />} />
         <Route path="/color-picker" element={<ColorPicker />} />
+        <Route path="/dark-mode" element={<DarkMode />} />
+        <Route path="/dog-pics" element={<DogPics />} />
+        <Route path="/focus-input" element={<FocusInput />} />
+        <Route path="/form-validator" element={<FormValidator />} />
+        <Route path="/pixel-art" element={<PixelArt />} />
+        <Route path="/score-keeper" element={<ScoreKeeper />} />
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
+        <Route path="/simple-calculator" element={<SimpleCalculator />} />
       </Routes>
     </div>
   );
