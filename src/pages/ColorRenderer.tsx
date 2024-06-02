6b61b75e2ca0879cffdca90c4abb4467b824b106
marinaaglic <marinaaglic11@gmail.com>
2024-06-02 22:00:24 +0200

    added task text and back button

--- src/pages/ColorRenderer.tsx
+++ src/pages/ColorRenderer.tsx
@@ -1,4 +1,5 @@
 import Color from "../components/Color";
+import { Link } from "react-router-dom";
 
 const colors = [
   {
@@ -21,11 +22,23 @@ const colors = [
 
 const ColorRenderer = () => {
   return (
-    <h2>
-      {colors.map((color) => (
-        <Color key={color.name} hex={color.hex} name={color.name} />
-      ))}
-    </h2>
+    <>
+      <h2>COLOR RENDERER</h2>
+      <p>
+        Build a color renderer that will display color swatches on a user
+        interface.
+      </p>
+      <p>
+        Update the ColorRenderer component to display one instance of the Color
+        component for each color in the array of colors.
+      </p>
+      <h2>
+        {colors.map((color) => (
+          <Color key={color.name} hex={color.hex} name={color.name} />
+        ))}
+      </h2>
+      <Link to="/">Back</Link>
+    </>
   );
 };
 
