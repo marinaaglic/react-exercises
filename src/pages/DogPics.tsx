74137122f99f280fbbfa951d568a907d04bbc1fc
marinaaglic <marinaaglic11@gmail.com>
2024-06-10 20:59:29 +0200

    added text for task dog pics

--- src/pages/DogPics.tsx
+++ src/pages/DogPics.tsx
@@ -1,12 +1,5 @@
-/* TASK
-Use a dog picture API to display dog images in React
-Reaquirements:
--Use the dog.ceo to fetch a random dog image and display 
-    that image on page load instead of the hardcoded one there now
--When the dog button is clicked, fetch a new dog image and render it on the UI instead
-*/
-
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 type ResponseType = {
   message: string;
@@ -34,8 +27,20 @@ const DogPics = () => {
   // API: https://dog.ceo/dog-api/
   return (
     <div className="dog-pics">
+      <h2>Dog pics</h2>
+      <p>Use a dog picture API to display dog images in React</p>
+      <p>Reaquirements:</p>
+      <p>
+        Use the dog.ceo to fetch a random dog image and display that image on
+        page load instead of the hardcoded one there now
+      </p>
+      <p>
+        When the dog button is clicked, fetch a new dog image and render it on
+        the UI instead
+      </p>
       {data.message && <img src={data.message} alt="A random dog" />}
       <button onClick={fetchDogPic}>🐶</button>
+      <Link to="/">Back</Link>
     </div>
   );
 };
