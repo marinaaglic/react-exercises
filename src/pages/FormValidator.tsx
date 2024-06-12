799ae7d2635035231e76e0148454d99bc37f28aa
marinaaglic <marinaaglic11@gmail.com>
2024-06-12 21:30:20 +0200

    added task text for form validator

--- src/pages/FormValidator.tsx
+++ src/pages/FormValidator.tsx
@@ -1,15 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-/* TASK 
-Validate a user sign-up formonce the user submits the form
-When the form is submitted, validate that the user's information fits the following requirements:
--the email, password, and password confirmation fields are filled in
--the email field must have exactly one @sign in it
--the password must be eight or more characters long
--the password and password confirmation must be the same
-If any of those conditions aren't met, display an error, if they are all met display a sucess message
-*/
 import { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
 
 const FormValidator = () => {
   const [email, setEmail] = useState("");
@@ -49,33 +39,52 @@ const FormValidator = () => {
   };
 
   return (
-    <form onSubmit={validateForm}>
-      <h2>Sign Up!</h2>
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        name="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <label htmlFor="password-confirm">Confirm Password </label>
-      <input
-        type="password"
-        name="password-confirm"
-        onChange={(e) => setPasswordConfirm(e.target.value)}
-        value={passwordConfirm}
-      />
-      <input type="submit" value="Submit" />
-      {errorMessage && <p>{errorMessage}</p>}
-      {successMessage && <p>{successMessage}</p>}
-    </form>
+    <>
+      <h2>FORM VALIDATOR</h2>
+      <p>Validate a user sign-up formonce the user submits the form</p>
+      <ul>
+        When the form is submitted, validate that the user's information fits
+        the following requirements:
+        <li>
+          the email, password, and password confirmation fields are filled in
+        </li>
+        <li>the email field must have exactly one @sign in it</li>
+        <li>the password must be eight or more characters long</li>
+        <li>the password and password confirmation must be the same</li>
+      </ul>
+      <p>
+        If any of those conditions aren't met, display an error, if they are all
+        met display a sucess message
+      </p>
+      <form onSubmit={validateForm}>
+        <h2>Sign Up!</h2>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <label htmlFor="password-confirm">Confirm Password </label>
+        <input
+          type="password"
+          name="password-confirm"
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+          value={passwordConfirm}
+        />
+        <input type="submit" value="Submit" />
+        {errorMessage && <p>{errorMessage}</p>}
+        {successMessage && <p>{successMessage}</p>}
+      </form>
+      <Link to="/">Back</Link>
+    </>
   );
 };
 
