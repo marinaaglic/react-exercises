27ac4f6a58c1c5666600cdb7836494ac37370ab8
marinaaglic <marinaaglic11@gmail.com>
2024-06-17 22:08:26 +0200

    added task text for pixel art

--- src/pages/PixelArt.tsx
+++ src/pages/PixelArt.tsx
@@ -1,29 +1,37 @@
-/* TASK
-Build a pixel art drawing app
-React Context 
--   Allows you to share data between components more easily. 
-    Instead of having to pass a prop down multiple levels, you can use Context.
-Requirements:
-- When a color is clicked, taht color becomes the drawing color. Then, when one of the grid
-    squares is clicked, it should be colored in with the drawing color.
-- Use React Context to share state between your components
-*/
-
 import { useState } from "react";
 import { ColorContext } from "../context/ColorContext";
 import ColorPicker from "../components/PixelArt/PixelColorPicker";
 import Pixels from "../components/PixelArt/Pixels";
+import { Link } from "react-router-dom";
 
 const PixelArt = () => {
   const [selectedColor, setSelectedColor] = useState("black");
 
   return (
-    <ColorContext.Provider
-      value={{ selectedColor, setColor: setSelectedColor }}
-    >
-      <ColorPicker />
-      <Pixels />
-    </ColorContext.Provider>
+    <div>
+      <h2>PIXEL ART</h2>
+      <p>Build a pixel art drawing app</p>
+      <ul>React Context </ul>
+      <li>Allows you to share data between components more easily. </li>
+      <li>
+        Instead of having to pass a prop down multiple levels, you can use
+        Context.
+      </li>
+      <p>Requirements:</p>
+      <p>
+        When a color is clicked, taht color becomes the drawing color. Then,
+        when one of the grid squares is clicked, it should be colored in with
+        the drawing color.
+      </p>
+      <p>Use React Context to share state between your components</p>
+      <ColorContext.Provider
+        value={{ selectedColor, setColor: setSelectedColor }}
+      >
+        <ColorPicker />
+        <Pixels />
+      </ColorContext.Provider>
+      <Link to="/">Back</Link>
+    </div>
   );
 };
 
